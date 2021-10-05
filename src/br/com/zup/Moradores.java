bcaa1b01d74b7e51b3e0210ce2fb81c871719136
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-04 23:53:22 -0300

    Concluindo estrutura

--- src/br/com/zup/Moradores.java
+++ src/br/com/zup/Moradores.java
@@ -1,4 +1,30 @@
 package br.com.zup;
 
-public class Moradores {
+public class Moradores extends Pessoa {
+    private double renda;
+
+    public Moradores(){
+
+    }
+
+    public Moradores(String nome, String cpf, String telefone, double renda) {
+        super(nome, cpf, telefone);
+        this.renda = renda;
+    }
+
+    public double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(double renda) {
+        this.renda = renda;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\n"+ super.toString());
+        retorno.append(("\n Renda: "+renda ));
+        return retorno.toString();
+    }
 }
