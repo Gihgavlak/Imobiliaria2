67be7c1ee0f29d2e4450401442545d426f81d7cf
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-04 23:40:41 -0300

    Estrutura da classe pessoa

--- src/br/com/zup/Pessoa.java
+++ src/br/com/zup/Pessoa.java
@@ -2,12 +2,45 @@ package br.com.zup;
 
 public abstract class Pessoa {
     private String nome;
-    private String Cpf;
+    private String cpf;
     private String telefone;
 
     public Pessoa(String nome, String cpf, String telefone) {
         this.nome = nome;
-        Cpf = cpf;
+        cpf = cpf;
         this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder retorno = new StringBuilder();
+      retorno.append("Nome: "+ nome);
+      retorno.append("CPF: "+cpf);
+      retorno.append("Telefone; "+ telefone);
+      return retorno.toString();
     }
 }
