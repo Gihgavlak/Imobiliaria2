44755b0ed132c562c5bf4ea4ba6f3f8f97b72d1b
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-05 00:01:24 -0300

    Concluindo estrutura

--- src/br/com/zup/FuncionarioResponsavel.java
+++ src/br/com/zup/FuncionarioResponsavel.java
@@ -1,4 +1,19 @@
 package br.com.zup;
 
-public class FuncionarioResponsavel {
+public class FuncionarioResponsavel extends Pessoa{
+
+    public FuncionarioResponsavel(){
+
+    }
+
+    public FuncionarioResponsavel(String nome, String cpf, String telefone) {
+        super(nome, cpf, telefone);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append(super.toString());
+        return retorno.toString();
+    }
 }
