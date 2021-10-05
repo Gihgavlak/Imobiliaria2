adc7977a2f948cccbb626d59a5a8ef0f16370d19
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-05 00:15:20 -0300

    Metodos, getters e setters

--- src/br/com/zup/Imovel.java
+++ src/br/com/zup/Imovel.java
@@ -1,5 +1,53 @@
 package br.com.zup;
 
-public class Imovel {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Imovel {
+    private String enderecoImovel;
+    private double valorAluguel;
+    private List<Moradores> moradoresDoImovel = new ArrayList<>();
+    private List<FuncionarioResponsavel> funcionarioImobiliaria= new ArrayList();
+
+    public Imovel() {
+    }
+
+    public Imovel(String enderecoImovel, double valorAluguel, List<Moradores> moradoresDoImovel, List<FuncionarioResponsavel> funcionarioImobiliaria) {
+        this.enderecoImovel = enderecoImovel;
+        this.valorAluguel = valorAluguel;
+        this.moradoresDoImovel = moradoresDoImovel;
+        this.funcionarioImobiliaria = funcionarioImobiliaria;
+    }
+
+    public String getEnderecoImovel() {
+        return enderecoImovel;
+    }
+
+    public void setEnderecoImovel(String enderecoImovel) {
+        this.enderecoImovel = enderecoImovel;
+    }
+
+    public double getValorAluguel() {
+        return valorAluguel;
+    }
+
+    public void setValorAluguel(double valorAluguel) {
+        this.valorAluguel = valorAluguel;
+    }
+
+    public List<Moradores> getMoradoresDoImovel() {
+        return moradoresDoImovel;
+    }
+
+    public void setMoradoresDoImovel(List<Moradores> moradoresDoImovel) {
+        this.moradoresDoImovel = moradoresDoImovel;
+    }
+
+    public List<FuncionarioResponsavel> getFuncionarioImobiliaria() {
+        return funcionarioImobiliaria;
+    }
+
+    public void setFuncionarioImobiliaria(List<FuncionarioResponsavel> funcionarioImobiliaria) {
+        this.funcionarioImobiliaria = funcionarioImobiliaria;
+    }
 }
