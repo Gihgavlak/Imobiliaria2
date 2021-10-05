6dcbbd89705a99a4d83f74fab4bf2f2456688929
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-10-05 10:04:07 -0300

    Finalizando Estrutura Basica

--- src/br/com/zup/Imobiliaria.java
+++ src/br/com/zup/Imobiliaria.java
@@ -1,4 +1,31 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imobiliaria {
+    private List<Imovel> imoveis = new ArrayList<>();
+
+    public Imobiliaria() {
+    }
+
+    public Imobiliaria(List<Imovel> imoveis) {
+        this.imoveis = imoveis;
+    }
+
+    public List<Imovel> getImoveis() {
+        return imoveis;
+    }
+
+    public void setImoveis(List<Imovel> imoveis) {
+        this.imoveis = imoveis;
+    }
+
+    @Override
+    public String toString() {
+       StringBuilder retorno = new StringBuilder();
+       retorno.append("\n Quantidade de imoveis: "+imoveis.size() );
+       retorno.append("\n"+imoveis);
+       return retorno.toString();
+    }
 }
